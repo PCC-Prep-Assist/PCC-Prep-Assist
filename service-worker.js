de6e227d8ad170c72e740e39f97a64695d1424fd
PCC-PrepAssist <cuneyt_tanyel.koc@roche.com>
2026-02-06 16:43:11 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "pcc-prep-assist-cache";
+
+const PRECACHE_URLS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./Exhibition%20Mode%20Pro.pdf",
+  "./ExhibitionMode_for%20pure_0.3_SW%200102.pdf"
+];
+
+
+self.addEventListener("message", (event) => {
+  const data = event.data;
+  if (data && (data.type === "SKIP_WAITING" || data === "SKIP_WAITING")) {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
